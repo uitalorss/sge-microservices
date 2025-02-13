0f8cda1af9d0476f192b05c404b97a8059d19b0a
Uítalo Souza <uitalo.urss@gmail.com>
2025-02-13 13:47:16 -0300

    atualização de lógica de autenticação

--- services/users/core/auth/auth.py
+++ services/users/core/auth/auth.py
@@ -1,14 +1,21 @@
+from datetime import datetime, timedelta
+
+import jwt
 from fastapi.security import HTTPBearer
+from pytz import timezone
 
 from core.configs import settings
 
-from pytz import timezone
-from datetime import timedelta, datetime
-import jwt
-
 oauth2_schema = HTTPBearer(bearerFormat="JWT", auto_error=False)
 
-def create_token(token_type: str, lifetime: timedelta, sub: str):
+
+def create_token(
+    token_type: str,
+    lifetime: timedelta,
+    sub: str,
+    data_type: str,
+    is_active: bool,
+):
     payload = {}
     timezone_ba = timezone("America/Bahia")
     expires_in = datetime.now(tz=timezone_ba) + lifetime
@@ -17,13 +24,17 @@ def create_token(token_type: str, lifetime: timedelta, sub: str):
     payload["exp"] = expires_in
     payload["iat"] = datetime.now(tz=timezone_ba)
     payload["sub"] = str(sub)
-
-    print(payload)
+    payload["scopes"] = data_type
+    payload["is_active"] = is_active
 
     return jwt.encode(payload, settings.JWT_KEY, algorithm=settings.ALGORITHM)
 
 
-def create_access_token(sub: str):
-    token = create_token(token_type="access_token", lifetime=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES), sub=sub)
-
-    return token
+def create_access_token(sub: str, data_type: str, is_active: bool):
+    return create_token(
+        token_type="access_token",
+        lifetime=timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES),
+        sub=sub,
+        data_type=data_type,
+        is_active=is_active,
+    )
--- services/users/core/auth/deps.py
+++ services/users/core/auth/deps.py
@@ -1,14 +1,19 @@
 from typing import AsyncGenerator
-from fastapi import Depends, HTTPException, status
-from sqlalchemy.ext.asyncio import AsyncSession
-from ..configs import settings
-from sqlalchemy import select
-import jwt
-from jwt import PyJWTError
-from core.database import Session
-from .auth import oauth2_schema
-from models.user_model import UserModel
 from uuid import UUID
+
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import SecurityScopes
+from jwt import PyJWTError
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.database import Session
+from models.user_model import User
+
+from ..configs import settings
+from .auth import oauth2_schema
+
 
 async def get_session() -> AsyncGenerator:
     session: AsyncSession = Session()
@@ -18,37 +23,115 @@ async def get_session() -> AsyncGenerator:
     finally:
         await session.close()
 
-async def get_current_user(db: AsyncSession = Depends(get_session), token: str = Depends(oauth2_schema)):
 
+async def get_current_user_main(
+    security_scopes: SecurityScopes,
+    check_active: bool,
+    db: AsyncSession = Depends(get_session),
+    token: str = Depends(oauth2_schema),
+):
     credential_exception: HTTPException = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Token de acesso inválido ou expirado.",
-        headers={"WWW-authenticate": "Bearer"}
+        headers={"WWW-authenticate": "Bearer"},
     )
 
     if token is None or not token.credentials:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, 
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Token de acesso não fornecido ou não autorizado.",
-            headers={"WWW-authenticate": "Bearer"}
+            headers={"WWW-authenticate": "Bearer"},
         )
-    
 
     try:
-        payload = jwt.decode(token.credentials, settings.JWT_KEY, algorithms=settings.ALGORITHM,options={"verify_aud": False})
+        payload = jwt.decode(
+            token.credentials,
+            settings.JWT_KEY,
+            algorithms=settings.ALGORITHM,
+            options={"verify_aud": False},
+        )
         user_id = payload.get("sub")
+        token_scopes = payload.get("scopes", [])
+        is_active = payload.get("is_active")
         if user_id is None:
             raise credential_exception
-    
+
+        if security_scopes.scopes:
+            is_valid_scope = False
+            for scope in security_scopes.scopes:
+                if scope in token_scopes:
+                    is_valid_scope = True
+                    break
+
+            if not is_valid_scope:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Ação não permitida para o perfil informado.",
+                )
+        if check_active:
+            if not is_active:
+                raise HTTPException(
+                    status_code=status.HTTP_403_FORBIDDEN,
+                    detail="Ação não permitida para usuários inativos.",
+                )
+
     except PyJWTError:
         raise credential_exception
-    
+
     async with db as session:
-        query = select(UserModel).filter(UserModel.id == UUID(user_id))
+        query = select(User).filter(User.id == UUID(user_id))
         result = await session.execute(query)
-        usuario: UserModel = result.scalars().unique().one_or_none()
+        usuario = result.scalars().unique().one_or_none()
 
         if usuario is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Usuário não encontrado")
-        
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Usuário não Autenticado",
+            )
         return usuario
+
+
+async def get_current_user_without_profile_check(
+    security_scopes: SecurityScopes,
+    db: AsyncSession = Depends(get_session),
+    token: str = Depends(oauth2_schema),
+):
+    return await get_current_user_main(
+        security_scopes=security_scopes, db=db, token=token, check_active=False
+    )
+
+
+async def get_current_user(
+    security_scopes: SecurityScopes,
+    db: AsyncSession = Depends(get_session),
+    token: str = Depends(oauth2_schema),
+):
+    return await get_current_user_main(
+        security_scopes=security_scopes, db=db, token=token, check_active=True
+    )
+
+
+async def get_current_profile(
+    token: str = Depends(oauth2_schema),
+):
+    try:
+        payload = jwt.decode(
+            token.credentials,
+            settings.JWT_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+
+        scopes = payload.get("scopes", [])
+        if not scopes:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token inválido ou perfil ausente",
+            )
+
+        return scopes
+    except PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token de acesso inválido ou expirado.",
+            headers={"WWW-authenticate": "Bearer"},
+        )
